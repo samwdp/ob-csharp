e785655ff6e99f25fe458b37ab26b232d0202e54
Sam Precious <samwdp@gmail.com>
2020-08-22 20:20:23 +0100

    IT WORKS

--- ob-csharp.el
+++ ob-csharp.el
@@ -19,21 +19,19 @@
 
 (defun org-babel-execute:csharp (body params)
   (let* ((processed-params (org-babel-process-params params))
-         (src-temp (org-babel-temp-file "csharp-src-" ".cs")))
+         (src-temp (org-babel-temp-file "csharp-src-")))
     (with-temp-file src-temp (insert body))
-    (let ((results (org-babel-eval
-                    (ob-csharp--build-script-run-command src-temp)
-                    "")))
-      (when results
-        (setq results (org-trim (org-remove-indentation results)))
-        (org-babel-reassemble-table
-         (org-babel-result-cond (cdr (assq :result-params params))
-           (org-babel-read results t)
-           (let ((tmp-file (org-babel-temp-file "csharp-")))
-             (with-temp-file tmp-file (insert results))
-             (org-babel-import-elisp-from-file tmp-file)))
-         nil
-         nil)))))
+    (let ((results (org-babel-eval (ob-csharp--build-script-run-command src-temp) "")))
+      (org-babel-reassemble-table
+       (org-babel-result-cond (cdr (assoc :result-params params))
+         (org-babel-read results)
+         (let ((tmp-file (org-babel-temp-file "c-")))
+           (with-temp-file tmp-file (insert results))
+           (org-babel-import-elisp-from-file tmp-file)))
+       (org-babel-pick-name
+        (cdr (assoc :colname-names params)) (cdr (assoc :colnames params)))
+       (org-babel-pick-name
+        (cdr (assoc :rowname-names params)) (cdr (assoc :rownames params)))))))
 
 (provide 'ob-csharp)
 ;;; ob-csharp.el ends here
