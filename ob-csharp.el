4cfc0b164be5455c87b59e346e1a33932a0e35f3
Sam Precious <samwdp@gmail.com>
2020-09-28 21:03:49 +0100

    Updated lisense and pacage stuff

--- ob-csharp.el
+++ ob-csharp.el
@@ -1,9 +1,28 @@
-;;; ob-cshrp.el --- org-babel functions for csharp evaluation
+;;; ob-csharp.el --- Org-babel functions for csharp evaluation -*- lexical-binding: t -*-
+;;; Copyright (C) 2020 Sam Precious
+
+;; Author: Sam Precious <samwdp@gmail.com>
+;; URL: http://github.com/samwdp/ob-csharp
+;; Keywords: docs org babel csharp
+;; Version: 0.0.1
+;; Created: 28th Sept 2020
+;; Package-Requires: ((org "8") (emacs "24.4"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;
-;; org-babel functions for rust evaluation
-;;
+;; Org-babel functions for csharp evaluation
 
 ;;; Code:
 (require 'ob)
